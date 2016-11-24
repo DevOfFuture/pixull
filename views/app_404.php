459577efa9a547b15ba27c379c564f13ba370cf3
Sonkeng Maldini <sdmg15@users.noreply.github.com>
2016-11-24 14:49:16 +0100

    Update app_404.php
    Adding image to illustrate the not found.

--- views/app_404.php
+++ views/app_404.php
@@ -9,7 +9,8 @@ Viewer::view('app_header');
 	<div class="error-page">
 		<h2 class="headline text-yellow" style="margin-top: 0;">404</h2>
 		<div class="error-content">
-			<h3 style="padding-top: 10px;"><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
+			<h3 style="padding-top: 10px;"><img src="http://zupimages.net/up/16/47/enlb.jpg" alt="ghost" title="ghost"></h3><br/> 
+			 <h3>Oops! Page not found.</h3>
 			<p>We could not find the page you were looking for.</p>
 			<a href="#" class="btn btn-warning" onclick="window.history.go(-1); return false;"><i class="fa fa-arrow-left"></i> Go Back</a>
 		</div>
